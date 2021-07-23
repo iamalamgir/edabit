f97aa8e8f192bea02708300edb0a7038d90696cb
Alamgir Hossain <mdalamgir.pro@gmail.com>
2021-07-23 20:14:12 +0600

    Is the Number Less than or Equal to Zero

---
+++ 0003.js
@@ -0,0 +1,9 @@
+// https://edabit.com/challenge/PTiLYyb4A69KZtBCg
+
+function lessThanOrEqualToZero(num) {
+    return num <= 0
+}
+
+console.log(lessThanOrEqualToZero(5)) // false
+console.log(lessThanOrEqualToZero(0)) // true
+console.log(lessThanOrEqualToZero(-2)) // true
